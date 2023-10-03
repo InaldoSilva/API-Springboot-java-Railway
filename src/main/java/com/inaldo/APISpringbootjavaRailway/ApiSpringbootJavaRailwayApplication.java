f1ae3bae687bbd522462360aa4b93bb4bd64b908
Inaldo Silva <104039452+InaldoSilva@users.noreply.github.com>
2023-10-02 23:29:03 -0300

    Initial commit

---
+++ src/main/java/com/inaldo/APISpringbootjavaRailway/ApiSpringbootJavaRailwayApplication.java
@@ -0,0 +1,13 @@
+package com.inaldo.APISpringbootjavaRailway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSpringbootJavaRailwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSpringbootJavaRailwayApplication.class, args);
+	}
+
+}
